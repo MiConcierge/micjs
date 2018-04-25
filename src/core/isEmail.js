8f6772fe23952d45bf66c91614193a7029ce93f5
Jerome Olvera <jerome.olvera@gmail.com>
2018-04-25 17:35:28 -0500

    upgraded missing data

--- src/core/isEmail.js
+++ src/core/isEmail.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-useless-escape */
 const EMAIL_REGEX = /^[\w!#\$%&'\*\+\/\=\?\^`\{\|\}~\-]+(:?\.[\w!#\$%&'\*\+\/\=\?\^`\{\|\}~\-]+)*@(?:[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?$/i
+/* eslint-enable no-useless-escape */
 
 export default function isEmail (email) {
   return EMAIL_REGEX.test(email)
