9a6fc18766b7873c3d4e9e7f7cee1546a0655501
Jerome Olvera <jerome.olvera@gmail.com>
2018-05-03 11:14:43 -0500

    fixed typo

--- src/core/reverseArray.js
+++ src/core/reverseArray.js
@@ -1,5 +1,5 @@
 /**
- * Expose a immutable version of Array.prototype.reverse
+ * Expose an immutable version of Array.prototype.reverse
  */
 export default function reverseArray (array) {
   return array.slice().reverse()
