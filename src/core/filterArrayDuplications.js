722f63182e76a95dc2c8e926892750156a5728d2
Jerome Olvera <jerome.olvera@gmail.com>
2018-07-15 13:36:37 -0500

    remove code reundancy

--- src/core/filterArrayDuplications.js
+++ src/core/filterArrayDuplications.js
@@ -1,17 +1,7 @@
-/**
- * Stringify will help to compare arrays of objects
- */
-export const _mapStringArray = x => JSON.stringify(x)
-
-/**
- * Should restore original array value
- */
-export const _mapParseArray = x => JSON.parse(x)
-
 export default function filterArrayDuplications (array) {
   /**
    * Convert an array to a set,
    * the return a new array without the duplications
    */
-  return [...new Set([ ...array.map(_mapStringArray) ])].map(_mapParseArray)
+  return [...new Set([ ...array.map(JSON.stringify) ])].map(JSON.parse)
 }
