85b68d707188c8e4d71f4ae8eba0573516e23497
Jerome Olvera <jerome.olvera@gmail.com>
2018-08-13 11:53:38 -0500

    updated cleanString function for handle regex

--- src/core/cleanString.js
+++ src/core/cleanString.js
@@ -1,12 +1,13 @@
-export default function cleanString (string = '') {
-  return string
+export default function cleanString (string) {
+  if (!string) throw new Error('No string')
+  return String(string)
+    /**
+     * Remove special caracters
+     */
+    .replace(/[^a-zA-Z0-9]/g, '')
     /**
      * Remove accents/diacritics
      */
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
-    /**
-     * Remove special caracters
-     */
-    .replace(/[^a-zA-Z0-9]/g, '')
 }
